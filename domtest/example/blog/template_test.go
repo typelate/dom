c76c815c90f29d5aa01a86017de9d8b68e726ccb
Christopher Hunter <8398225+crhntr@users.noreply.github.com>
2025-09-02 12:29:46 -0700

    fix(example): remove domtest.Case types from example code

--- domtest/example/blog/template_test.go
+++ domtest/example/blog/template_test.go
@@ -11,27 +11,35 @@ import (
 	"golang.org/x/net/html/atom"
 
 	"github.com/typelate/dom/domtest"
-	"github.com/typelate/dom/spec"
-
 	"github.com/typelate/dom/domtest/example/blog"
 	"github.com/typelate/dom/domtest/example/blog/internal/fake"
 )
 
 func TestCase(t *testing.T) {
-	for _, tt := range []domtest.Case[*testing.T, *fake.App]{
+	type (
+		Case struct {
+			Name  string
+			Given func(*testing.T, *fake.App)
+			When  func(*testing.T) *http.Request
+			Then  func(*testing.T, *http.Response, *fake.App)
+		}
+	)
+
+	for _, tt := range []Case{
 		{
 			Name: "viewing the home page",
-			Given: domtest.GivenPtr(func(t *testing.T, app *fake.App) {
+			Given: func(t *testing.T, app *fake.App) {
 				app.ArticleReturns(blog.Article{
 					Title:   "Greetings!",
 					Content: "Hello, friends!",
 					Error:   nil,
 				})
-			}),
+			},
 			When: func(t *testing.T) *http.Request {
 				return httptest.NewRequest(http.MethodGet, "/article/1", nil)
 			},
-			Then: domtest.Document(func(t *testing.T, document spec.Document, app *fake.App) {
+			Then: func(t *testing.T, response *http.Response, app *fake.App) {
+				document := domtest.ParseResponseDocument(t, response)
 				require.Equal(t, 1, app.ArticleArgsForCall(0))
 				if heading := document.QuerySelector("h1"); assert.NotNil(t, heading) {
 					require.Equal(t, "Greetings!", heading.TextContent())
@@ -39,41 +47,43 @@ func TestCase(t *testing.T) {
 				if content := document.QuerySelector("p"); assert.NotNil(t, content) {
 					require.Equal(t, "Hello, friends!", content.TextContent())
 				}
-			}),
+			},
 		},
 		{
 			Name: "the page has an error",
-			Given: func(t *testing.T) *fake.App { // GivenPtr removes some of the boilerplate in the block
-				app := new(fake.App)
+			Given: func(t *testing.T, app *fake.App) { // GivenPtr removes some of the boilerplate in the block
 				app.ArticleReturns(blog.Article{
 					Error: fmt.Errorf("lemon"),
 				})
-				return app
 			},
 			When: func(t *testing.T) *http.Request {
 				return httptest.NewRequest(http.MethodGet, "/article/1", nil)
 			},
-			Then: domtest.QuerySelector("#error-message", func(t *testing.T, msg spec.Element, app *fake.App) {
-				require.Equal(t, "lemon", msg.TextContent())
-			}),
+			Then: func(t *testing.T, response *http.Response, app *fake.App) {
+				document := domtest.ParseResponseDocument(t, response)
+				if msg := document.QuerySelector("#error-message"); assert.NotNil(t, msg) {
+					require.Equal(t, "lemon", msg.TextContent())
+				}
+			},
 		},
 		{
 			Name: "the page has an error and is requested by HTMX",
-			Given: domtest.GivenPtr(func(t *testing.T, app *fake.App) {
+			Given: func(t *testing.T, app *fake.App) {
 				app.ArticleReturns(blog.Article{
 					Error: fmt.Errorf("lemon"),
 				})
-			}),
+			},
 			When: func(t *testing.T) *http.Request {
 				req := httptest.NewRequest(http.MethodGet, "/article/1", nil)
 				req.Header.Set("HX-Request", "true")
 				return req
 			},
-			Then: domtest.Fragment(atom.Body, func(t *testing.T, fragment spec.DocumentFragment, app *fake.App) {
+			Then: func(t *testing.T, response *http.Response, app *fake.App) {
+				fragment := domtest.ParseResponseDocumentFragment(t, response, atom.Div)
 				el := fragment.FirstElementChild()
 				require.Equal(t, "lemon", el.TextContent())
 				require.Equal(t, "*errors.errorString", el.GetAttribute("data-type"))
-			}),
+			},
 		},
 		{
 			Name: "when the id is not an integer",
@@ -85,10 +95,22 @@ func TestCase(t *testing.T) {
 			},
 		},
 	} {
-		t.Run(tt.Name, tt.Run(func(fakes *fake.App) http.Handler {
+		t.Run(tt.Name, func(t *testing.T) {
+			app := new(fake.App)
+
+			if tt.Given != nil {
+				tt.Given(t, app)
+			}
+
+			req := tt.When(t)
+
 			mux := http.NewServeMux()
-			blog.TemplateRoutes(mux, fakes)
-			return mux
-		}))
+			blog.TemplateRoutes(mux, app)
+
+			rec := httptest.NewRecorder()
+			mux.ServeHTTP(rec, req)
+
+			tt.Then(t, rec.Result(), app)
+		})
 	}
 }
