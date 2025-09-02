d4e33c37983b1c994f2cde3f376c6f78aa60cc6a
Christopher Hunter <8398225+crhntr@users.noreply.github.com>
2025-09-02 12:43:54 -0700

    refactor(example): use struct parameters for test cases
    this makes adding colaborators easier
    it takes the fieldlist for each step and makes it extensible
    each case is no longer fragile to changes for new cases

--- domtest/example/blog/template_test.go
+++ domtest/example/blog/template_test.go
@@ -15,32 +15,61 @@ import (
 	"github.com/typelate/dom/domtest/example/blog/internal/fake"
 )
 
-func TestCase(t *testing.T) {
+func TestAPI(t *testing.T) {
 	type (
+		Given struct {
+			app *fake.App
+		}
+		When struct{}
+		Then struct {
+			app *fake.App
+		}
 		Case struct {
 			Name  string
-			Given func(*testing.T, *fake.App)
-			When  func(*testing.T) *http.Request
-			Then  func(*testing.T, *http.Response, *fake.App)
+			Given func(*testing.T, Given)
+			When  func(*testing.T, When) *http.Request
+			Then  func(*testing.T, *http.Response, Then)
 		}
 	)
+
+	run := func(t *testing.T, tc Case) {
+		app := new(fake.App)
+
+		if tc.Given != nil {
+			tc.Given(t, Given{
+				app: app,
+			})
+		}
+
+		req := tc.When(t, When{})
+
+		mux := http.NewServeMux()
+		blog.TemplateRoutes(mux, app)
+
+		rec := httptest.NewRecorder()
+		mux.ServeHTTP(rec, req)
+
+		tc.Then(t, rec.Result(), Then{
+			app: app,
+		})
+	}
 
 	for _, tt := range []Case{
 		{
 			Name: "viewing the home page",
-			Given: func(t *testing.T, app *fake.App) {
-				app.ArticleReturns(blog.Article{
+			Given: func(t *testing.T, given Given) {
+				given.app.ArticleReturns(blog.Article{
 					Title:   "Greetings!",
 					Content: "Hello, friends!",
 					Error:   nil,
 				})
 			},
-			When: func(t *testing.T) *http.Request {
+			When: func(t *testing.T, _ When) *http.Request {
 				return httptest.NewRequest(http.MethodGet, "/article/1", nil)
 			},
-			Then: func(t *testing.T, response *http.Response, app *fake.App) {
+			Then: func(t *testing.T, response *http.Response, then Then) {
 				document := domtest.ParseResponseDocument(t, response)
-				require.Equal(t, 1, app.ArticleArgsForCall(0))
+				require.Equal(t, 1, then.app.ArticleArgsForCall(0))
 				if heading := document.QuerySelector("h1"); assert.NotNil(t, heading) {
 					require.Equal(t, "Greetings!", heading.TextContent())
 				}
@@ -51,15 +80,15 @@ func TestCase(t *testing.T) {
 		},
 		{
 			Name: "the page has an error",
-			Given: func(t *testing.T, app *fake.App) { // GivenPtr removes some of the boilerplate in the block
-				app.ArticleReturns(blog.Article{
+			Given: func(t *testing.T, given Given) { // GivenPtr removes some of the boilerplate in the block
+				given.app.ArticleReturns(blog.Article{
 					Error: fmt.Errorf("lemon"),
 				})
 			},
-			When: func(t *testing.T) *http.Request {
+			When: func(t *testing.T, when When) *http.Request {
 				return httptest.NewRequest(http.MethodGet, "/article/1", nil)
 			},
-			Then: func(t *testing.T, response *http.Response, app *fake.App) {
+			Then: func(t *testing.T, response *http.Response, then Then) {
 				document := domtest.ParseResponseDocument(t, response)
 				if msg := document.QuerySelector("#error-message"); assert.NotNil(t, msg) {
 					require.Equal(t, "lemon", msg.TextContent())
@@ -68,17 +97,17 @@ func TestCase(t *testing.T) {
 		},
 		{
 			Name: "the page has an error and is requested by HTMX",
-			Given: func(t *testing.T, app *fake.App) {
-				app.ArticleReturns(blog.Article{
+			Given: func(t *testing.T, given Given) {
+				given.app.ArticleReturns(blog.Article{
 					Error: fmt.Errorf("lemon"),
 				})
 			},
-			When: func(t *testing.T) *http.Request {
+			When: func(t *testing.T, _ When) *http.Request {
 				req := httptest.NewRequest(http.MethodGet, "/article/1", nil)
 				req.Header.Set("HX-Request", "true")
 				return req
 			},
-			Then: func(t *testing.T, response *http.Response, app *fake.App) {
+			Then: func(t *testing.T, response *http.Response, _ Then) {
 				fragment := domtest.ParseResponseDocumentFragment(t, response, atom.Div)
 				el := fragment.FirstElementChild()
 				require.Equal(t, "lemon", el.TextContent())
@@ -87,30 +116,16 @@ func TestCase(t *testing.T) {
 		},
 		{
 			Name: "when the id is not an integer",
-			When: func(t *testing.T) *http.Request {
+			When: func(t *testing.T, _ When) *http.Request {
 				return httptest.NewRequest(http.MethodGet, "/article/banana", nil)
 			},
-			Then: func(t *testing.T, res *http.Response, f *fake.App) {
+			Then: func(t *testing.T, res *http.Response, _ Then) {
 				require.Equal(t, http.StatusBadRequest, res.StatusCode)
 			},
 		},
 	} {
 		t.Run(tt.Name, func(t *testing.T) {
-			app := new(fake.App)
-
-			if tt.Given != nil {
-				tt.Given(t, app)
-			}
-
-			req := tt.When(t)
-
-			mux := http.NewServeMux()
-			blog.TemplateRoutes(mux, app)
-
-			rec := httptest.NewRecorder()
-			mux.ServeHTTP(rec, req)
-
-			tt.Then(t, rec.Result(), app)
+			run(t, tt)
 		})
 	}
 }
